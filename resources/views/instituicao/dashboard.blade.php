8290b6e6f95b9cdfecaabb3b825b9794cf85e600
JoaoFelipeNascimentoLopes <joaofelipe.nl05@gmail.com>
2025-04-24 15:31:14 -0300

    fix: Design Dashboard (Instituição)

--- resources/views/instituicao/dashboard.blade.php
+++ resources/views/instituicao/dashboard.blade.php
@@ -4,7 +4,155 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <!-- Adiciona Arquivos CSS e JS Externos com o Vite -->
+    @vite(['resources/js/app.js'])
+    <link rel="icon" href="{{ asset('images/page_Icon.png') }}" type="image/png">
+    <script src="https://unpkg.com/@tailwindcss/browser@4"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0&icon_names=history_edu" />
+    <link
+        href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+        rel="stylesheet" />
+    <title>Dashboard - Instituição</title>
+    <style>
+        .poppins-thin {
+            font-family: "Poppins", serif;
+            font-weight: 100;
+            font-style: normal;
+        }
+
+        .poppins-extralight {
+            font-family: "Poppins", serif;
+            font-weight: 200;
+            font-style: normal;
+        }
+
+        .poppins-light {
+            font-family: "Poppins", serif;
+            font-weight: 300;
+            font-style: normal;
+        }
+
+        .poppins-regular {
+            font-family: "Poppins", serif;
+            font-weight: 400;
+            font-style: normal;
+        }
+
+        .poppins-medium {
+            font-family: "Poppins", serif;
+            font-weight: 500;
+            font-style: normal;
+        }
+
+        .poppins-semibold {
+            font-family: "Poppins", serif;
+            font-weight: 600;
+            font-style: normal;
+        }
+
+        .poppins-bold {
+            font-family: "Poppins", serif;
+            font-weight: 700;
+            font-style: normal;
+        }
+
+        .poppins-extrabold {
+            font-family: "Poppins", serif;
+            font-weight: 800;
+            font-style: normal;
+        }
+
+        .poppins-black {
+            font-family: "Poppins", serif;
+            font-weight: 900;
+            font-style: normal;
+        }
+
+        .poppins-thin-italic {
+            font-family: "Poppins", serif;
+            font-weight: 100;
+            font-style: italic;
+        }
+
+        .poppins-extralight-italic {
+            font-family: "Poppins", serif;
+            font-weight: 200;
+            font-style: italic;
+        }
+
+        .poppins-light-italic {
+            font-family: "Poppins", serif;
+            font-weight: 300;
+            font-style: italic;
+        }
+
+        .poppins-regular-italic {
+            font-family: "Poppins", serif;
+            font-weight: 400;
+            font-style: italic;
+        }
+
+        .poppins-medium-italic {
+            font-family: "Poppins", serif;
+            font-weight: 500;
+            font-style: italic;
+        }
+
+        .poppins-semibold-italic {
+            font-family: "Poppins", serif;
+            font-weight: 600;
+            font-style: italic;
+        }
+
+        .poppins-bold-italic {
+            font-family: "Poppins", serif;
+            font-weight: 700;
+            font-style: italic;
+        }
+
+        .poppins-extrabold-italic {
+            font-family: "Poppins", serif;
+            font-weight: 800;
+            font-style: italic;
+        }
+
+        .poppins-black-italic {
+            font-family: "Poppins", serif;
+            font-weight: 900;
+            font-style: italic;
+        }
+    </style>
+    <!-- Customização do Tailwind CSS -->
+    <style type="text/tailwindcss">
+        .ml-percent-15 {
+            margin-left: 15%;
+        }
+
+        .ml-percent-5 {
+            margin-left: 5%;
+        }
+
+        .ml-percent-1 {
+            margin-left: 1%;
+        }
+
+        .mr-percent-1 {
+            margin-right: 1%;
+        }
+
+        .mr-percent-5 {
+            margin-right: 5%;
+        }
+
+        .mr-percent-10 {
+            margin-right: 10%;
+        }
+
+        .div-flex {
+            display: flex;
+        }
+    </style>
 </head>
 <body>
     Dashboard Instituição
@@ -12,6 +160,5 @@
     <form action="{{ route('logout') }}" method="GET" style="display:inline;">
         <button type="submit">Sair</button>
     </form>
-    
 </body>
 </html>
